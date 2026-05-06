7375decf153e171992640e0a9435eb582c57ac62
P47K0 <pkoorevaar@hotmail.com>
2026-05-06 22:09:10 +0200

    Update variables.tf

--- .github/workflows/terraform/variables.tf
+++ .github/workflows/terraform/variables.tf
@@ -3,6 +3,11 @@ variable "location" {
   default     = "eastus2"
 }
 
+variable "resource_group_name" {
+  description = "Resource Group name"
+  default     = "koorevaar-rg"
+}
+
 variable "dockerhub_username" {
   description = "Your Docker Hub username"
   type        = string
