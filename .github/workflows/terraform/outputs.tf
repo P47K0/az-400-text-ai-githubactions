cf0fdd6aaf5c558cd2ac4e4d6fc7b48161e28437
P47K0 <pkoorevaar@hotmail.com>
2026-05-06 02:11:31 +0200

    Add Terraform outputs for resource group and APIs

---
+++ .github/workflows/terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "container_app_environment_name" {
+  value = azurerm_container_app_environment.env.name
+}
+
+output "text_intelligence_api_url" {
+  value = "https://${azurerm_container_app.text_api.ingress[0].fqdn}"
+}
+
+output "llm_adapter_internal_url" {
+  value = "http://llm-adapter"
+}
+
+output "important_notes" {
+  value = "Update OLLAMA_URL in the text_api container with your Cloudflare Worker URL"
+}
