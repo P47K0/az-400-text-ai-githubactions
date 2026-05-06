808bead7eaa2fd625ba2fc451be4f39c380eb842
P47K0 <pkoorevaar@hotmail.com>
2026-05-06 22:05:49 +0200

    Remove resource_group_name variable
    Removed the resource_group_name variable from Terraform configuration.

--- .github/workflows/terraform/variables.tf
+++ .github/workflows/terraform/variables.tf
@@ -3,11 +3,6 @@ variable "location" {
   default     = "eastus2"
 }
 
-variable "resource_group_name" {
-  description = "Resource Group name"
-  default     = "koorevaar-rg"
-}
-
 variable "dockerhub_username" {
   description = "Your Docker Hub username"
   type        = string
